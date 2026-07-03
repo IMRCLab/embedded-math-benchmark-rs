59fc0b2a1989c97b7e6ab4919fcfa37890c037f2
pascalharp <6959837+pascalharp@users.noreply.github.com>
2026-07-03 18:21:00 +0200

    chore(benchmarks): remove micromath from MatMul3x3

--- benchmarks/mrs-benchmark-core/src/suites/micromath.rs
+++ benchmarks/mrs-benchmark-core/src/suites/micromath.rs
@@ -1,48 +1,8 @@
-use crate::tasks::{MatMul3x3 as MatMul3x3Task, RotateVector as RotateVectorTask};
+use crate::tasks::RotateVector as RotateVectorTask;
 use crate::TaskImplementation;
-use micromath::{Quaternion, F32};
+use micromath::Quaternion;
 
-pub struct MatMul3x3;
 
-impl TaskImplementation<MatMul3x3Task> for MatMul3x3 {
-    const LIBRARY_IDENTIFIER: &'static str = "micromath";
-
-    type PreparedInput = ([F32; 9], [F32; 9]);
-    type RawOutput = [f32; 9];
-
-    fn prepare(&self, input: &<MatMul3x3Task as crate::BenchmarkTask>::Input) -> Self::PreparedInput {
-        let mut lhs = [F32(0.0); 9];
-        let mut rhs = [F32(0.0); 9];
-        for i in 0..9 {
-            lhs[i] = F32(input.lhs[i]);
-            rhs[i] = F32(input.rhs[i]);
-        }
-        (lhs, rhs)
-    }
-
-    fn execute(&self, input: &Self::PreparedInput) -> Self::RawOutput {
-        let a = &input.0;
-        let b = &input.1;
-
-        let r0 = a[0] * b[0] + a[1] * b[3] + a[2] * b[6];
-        let r1 = a[0] * b[1] + a[1] * b[4] + a[2] * b[7];
-        let r2 = a[0] * b[2] + a[1] * b[5] + a[2] * b[8];
-
-        let r3 = a[3] * b[0] + a[4] * b[3] + a[5] * b[6];
-        let r4 = a[3] * b[1] + a[4] * b[4] + a[5] * b[7];
-        let r5 = a[3] * b[2] + a[4] * b[5] + a[5] * b[8];
-
-        let r6 = a[6] * b[0] + a[7] * b[3] + a[8] * b[6];
-        let r7 = a[6] * b[1] + a[7] * b[4] + a[8] * b[7];
-        let r8 = a[6] * b[2] + a[7] * b[5] + a[8] * b[8];
-
-        [r0.0, r1.0, r2.0, r3.0, r4.0, r5.0, r6.0, r7.0, r8.0]
-    }
-
-    fn finalize(&self, output: Self::RawOutput) -> <MatMul3x3Task as crate::BenchmarkTask>::Output {
-        output
-    }
-}
 
 pub struct RotateVector;
 
